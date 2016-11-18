5fe715e2e20dd46e9dd19e1a3400ffa8d48535d2
mamidi varun <mamidi varun>
2016-11-18 17:09:59 +0530

    Added New File Vijjesh

---
+++ src/main/java/sample/Vijjesh.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Vijjesh {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
